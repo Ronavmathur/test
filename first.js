b98e265f5cce1f4b996fc18351cbd7780d73e620
ronav <Ronavmathur10@hotmail.com>
2026-01-04 21:37:31 -0500

    made a vowel checker

--- first.js
+++ first.js
@@ -1,2 +1,9 @@
-// hello
-console.log('first')
+function countVowels(s) {
+  let count = 0;
+  let vowels = "aeiouAEIOU";
+
+  for (let i = 0; i < s.length; i++) {
+    if (vowels.includes(s[i])) count++;
+  }
+  return count;
+}
